93d399cb33dcb62601d8eac156d1d59995fffa39
Yan Drugalya <ydrugalya@gmail.com>
2017-05-24 15:21:00 +0200

    continue implementing autosuggest

--- src/app/components/smartcomplete/smartcomplete.component.tsx
+++ src/app/components/smartcomplete/smartcomplete.component.tsx
@@ -4,6 +4,8 @@ import Autosuggest = require('react-autosuggest');
 import ShadowDOM from 'react-shadow';
 import { CommandLine } from "./../../CommandLine";
 import { Search } from "./../../Search"
+import { Utr } from "./../../Utr"
+
 var match = require('autosuggest-highlight/match');
 var parse = require('autosuggest-highlight/parse');
 
@@ -12,6 +14,7 @@ export interface CommandSelectedCallback { (data: string) : void }
 interface SmartCompleteProps {
     commands: CommandLine[]
     commandSelectedCallBack: CommandSelectedCallback
+    renderingParamsCompletions?: boolean
 }
 
 function escapeRegexCharacters(str: string): string {
@@ -26,15 +29,34 @@ export class SmartComplete extends React.Component<SmartCompleteProps, any> {
         this.state = {
             value: '',
             suggestions: this.getSuggestions(''),
-            searchedKeywords: []
+            searchedKeywords: [],
+            renderingParamsCompletions: false
         };
     }
- 
+    private keyDown(event: React.FormEvent<any>) {
+        const keyboardEvent = event.nativeEvent as KeyboardEvent;
+        if (keyboardEvent != null && keyboardEvent.ctrlKey && keyboardEvent.keyCode == 32) {
+            Utr.complete(this.state.value, this.completionsAvalaibe.bind(this)); 
+        }
+    }
+
+    private completionsAvalaibe(complettions: Array<string>): void {
+        const suggestions : CommandLine[] = new Array<CommandLine>();
+        complettions.forEach(c => {
+            suggestions.push({cmd: `${c}`} )
+        });
+        this.setState({
+            suggestions: suggestions,
+            renderingParamsCompletions: true
+        });
+    }
+
     render(): JSX.Element {
         const {value, suggestions} = this.state;
         const inputProps = {
             placeholder: `Type what you remember: 'native', 'integration', 'physics'. Select the command and hit enter`,
             value,
+            onKeyDown: this.keyDown.bind(this),
             autoFocus: true,
             onChange: this
                 .onChange
@@ -45,6 +67,7 @@ export class SmartComplete extends React.Component<SmartCompleteProps, any> {
                 <div> 
                     <style>{this.style}</style>
                     <Autosuggest
+                   
                     suggestions={suggestions}
                     onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
                     getSuggestionValue={this.getSuggestionValue}
@@ -58,7 +81,24 @@ export class SmartComplete extends React.Component<SmartCompleteProps, any> {
     }
 
     protected onSuggestionsSelected(event: React.FormEvent<any>, data: Autosuggest.SuggestionSelectedEventData<CommandLine>): void {
-        this.props.commandSelectedCallBack(data.suggestion.cmd);
+        if (this.state.renderingParamsCompletions) {
+            console.log('renderingParamsCompletions is true ' + this.state.value);
+            this.props.commandSelectedCallBack(this.state.value + ' ' + data.suggestion.cmd);
+            this.setState ({
+                value: this.adjustCompletionValueForInput(this.state.value, data.suggestion.cmd),
+                renderingParamsCompletions: false
+            });
+        } else {
+            this.props.commandSelectedCallBack(data.suggestion.cmd);
+        }   
+    }
+
+    protected adjustCompletionValueForInput(value: string, completion: string) : string {
+        var result = value;
+        var parts = result.split(' ');
+        parts.pop();
+
+        return parts.join(' ') + ' ' + completion;
     }
 
     protected onSuggestionsClearRequested() {
