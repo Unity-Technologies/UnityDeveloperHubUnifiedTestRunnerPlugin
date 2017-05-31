98a1c6fff6c25080b6b5767a0a7c51160a7d4d2d
Yan Drugalya <ydrugalya@gmail.com>
2017-05-31 10:57:44 +0300

    added line splitter helper for log

--- src/app/__tests__/log/log.utils.test.ts
+++ src/app/__tests__/log/log.utils.test.ts
@@ -4,12 +4,49 @@ import { assert } from 'chai'
 describe('log.utils tests', () => {
     it('empty line. returns 0 parts', () => {
         const parts = LogUtils.split('');
-        assert(parts.length == 0);
+       assert.deepEqual(parts, []);
     });
 
     it('line does not contain a file name. returns 1 part', () => {
-        const parts = LogUtils.split('abc');
-    
-        assert.deepEqual(parts, [{value: 'abc', isFileName: false}]);
+        const parts = LogUtils.split('string');
+        assert.deepEqual(parts, [{value: 'string', isFileName: false}]);
+    });
+
+    it('line contains file name only. returns 1 part with file name flag set', () => {
+        ['/file', '/tmp/file.txt', 'c:\\file'].forEach((value => {
+            const parts = LogUtils.split(value);
+            assert.deepEqual(parts, [{value: value, isFileName: true}]);
+        }))
+    });
+
+    it('line contains both: file name and some random text. returns 2 parts with with properly set isFileName flag', () => {
+        const parts = LogUtils.split('some text /file.txt');
+        assert.deepEqual(parts, [ 
+            {value: 'some text', isFileName: false},
+            {value: ' ', isFileName: false},
+            {value: '/file.txt', isFileName: true}
+        ]);
+    });
+
+    it('line contains 2 file names. returns 3 parts with properly set isFileName flag', () => {
+        const parts = LogUtils.split('/file1 /file2');
+        assert.deepEqual(parts, [ 
+            {value: '/file1', isFileName: true},
+            {value: ' ', isFileName: false},
+            {value: '/file2', isFileName: true}
+        ]);
+    });
+
+    it('line contains 2 file names and text in the beggining and end. returns 3 parts with properly set isFileName flag', () => {
+        const parts = LogUtils.split('prefix text /file /file suffix text');
+        console.log(parts);
+        assert.deepEqual(parts, [ 
+            {value: 'prefix text', isFileName: false},
+            {value: ' ', isFileName: false},
+            {value: '/file', isFileName: true},
+            {value: ' ', isFileName: false},
+            {value: '/file', isFileName: true},
+            {value: ' suffix text', isFileName: false},
+        ]);
     });
 });
--- src/app/components/log/log.utils.ts
+++ src/app/components/log/log.utils.ts
@@ -4,12 +4,24 @@ export interface LinePart {
 }
 
 export class LogUtils {
-    public static split(line: string): LinePart[]
-    {
+    
+    public static split(line: string): LinePart[] {
         const result = new Array<LinePart>();
-        if (line.length != 0) {
-            result.push ({value: line, isFileName: false}) 
+        if (line == null || line.length == null) {
+            return result;
         }
+        const regex = /(\s+|^)((?:[a-zA-Z]\:){0,1}(?:[\\/][\w.]+){1,})/g;
+        const parts = line.split(regex);
+        parts.forEach(p => {
+            if (p === '') {
+                return;
+            }
+            if (p.match(regex)) {
+                result.push({ value: p, isFileName: true })
+            } else {
+                result.push({ value: p, isFileName: false })
+            }
+        });
         return result;
     }
 }
