38dcf1cd6a3c428d8554d00abbaac564af93ca2c
Yan Drugalya <ydrugalya@gmail.com>
2017-06-05 15:56:32 +0300

    reformat

--- src/app/Search.ts
+++ src/app/Search.ts
@@ -1,26 +1,26 @@
 import { CommandLine } from "./CommandLine"
 
-const request = require ('request')
+const request = require('request')
 
-export interface SuggestionSearchRequest {
+export interfa  ce SuggestionSearchRequest {
     data: CommandLine[]
     keywords: string[]
 }
 
 export class Search {
-    static suggest(params: SuggestionSearchRequest, callback: any) : void {
-        var regexpCond = params.keywords.map(p => '(.*' + p +'.*)').join('');
+    static suggest(params: SuggestionSearchRequest, callback: any): void {
+        var regexpCond = params.keywords.map(p => '(.*' + p + '.*)').join('');
         var regExp = new RegExp(regexpCond, 'i');
-        var matches = params.data.filter( e => regExp.test(e.cmd)).slice(0, 10) ;
+        var matches = params.data.filter(e => regExp.test(e.cmd)).slice(0, 10);
         if (matches.length > 0) {
-            callback (null, matches);
+            callback(null, matches);
             return;
         }
 
-        Search.httpGet(params, function(err, data) {
+        Search.httpGet(params, function (err, data) {
             const result = new Array<CommandLine>();
             data.forEach(e => {
-                const cmd : CommandLine = {
+                const cmd: CommandLine = {
                     cmd: `--suite=${e.Suite} --testfilter=${e.Name}`
                 }
                 result.push(cmd);
@@ -29,12 +29,12 @@ export class Search {
         });
     }
 
-    static httpGet(params: SuggestionSearchRequest, callback: any) : void {
+    static httpGet(params: SuggestionSearchRequest, callback: any): void {
         const testName = params.keywords[0];
         const url = `http://api.hoarder.qa-staging.hq.unity3d.com/v1/TestInfo`;
         var propertiesObject = { name: testName };
         const result = new Array<CommandLine>();
-        request.get ({url:url, qs:propertiesObject}, function(err, response, body) {
+        request.get({ url: url, qs: propertiesObject }, function (err, response, body) {
             if (err) {
                 console.error(err);
                 return;
@@ -44,7 +44,7 @@ export class Search {
             if (data == undefined) {
                 data = [];
             }
-            callback (null, data);
+            callback(null, data);
         });
     }
 }
