20c5234e844f94aad702e014aa674b0cc84c9f98
Yan Drugalya <ydrugalya@gmail.com>
2017-06-07 09:23:21 +0300

    more tests for utr

--- src/app/__tests__/utr.test.ts
+++ src/app/__tests__/utr.test.ts
@@ -1,25 +1,47 @@
-import {assert} from 'chai'
+import { assert } from 'chai'
 import { Utr, HistoryCallback } from '../Utr'
 import * as mock_spawn from 'mock_spawn';
-import {AppSettings} from '../AppSettings'
-
-
+import { AppSettings } from '../AppSettings'
 
 describe('UTR tests', () => {
     describe("run", () => {
         it('run invokes correct command line and sets proper current directory', function () {
-            var {utr, spawn} = _makeUtr();
-            utr.run('--foo=bar', ()=> {}, ()=> {});
+            var { utr, spawn } = _makeUtr();
+            utr.run('--foo=bar', () => { }, () => { });
             var firstCall = spawn.calls[0];
             assert.equal('perl', firstCall.command);
             assert.deepEqual(['utr.pl', '--foo=bar'], firstCall.args);
-            assert.deepEqual({cwd: 'repo_root'}, firstCall.opts);
-	    })
+            assert.deepEqual({ cwd: 'repo_root' }, firstCall.opts);
+        })
+
+        it('emits stdout', function (done) {
+            var exitCode = 0;
+            var stdOut = 'output'
+            var { utr, spawn } = _makeUtr();
+            spawn.setDefault(spawn.simple(exitCode, stdOut));
+            utr.run('--foo=bar', (line: string) => {
+                assert.equal(line, stdOut);
+                done();
+            }, () => { });
+        });
+
+        it('emits stderr', function (done) {
+            var exitCode = 0;
+            var stdErr = 'output'
+            var { utr, spawn } = _makeUtr();
+            spawn.setDefault(spawn.simple(exitCode, '', stdErr));
+            utr.run('--foo=bar',
+                () => { },
+                (line: string) => {
+                    assert.equal(line, stdErr);
+                    done();
+                });
+        });
     })
 
     describe("history", () => {
         it('invokes with correct arguments', (done) => {
-            var {utr, spawn} = _makeUtr();
+            var { utr, spawn } = _makeUtr();
             utr.history((histEntries: Array<string>) => {
                 var call = spawn.calls[0];
                 assert.equal(call.command, 'perl');
@@ -29,8 +51,8 @@ describe('UTR tests', () => {
             });
         });
 
-       it('Invokes callback with the list of strings strings', (done) => {
-            var {utr, spawn} = _makeUtr();
+        it('Invokes callback with the list of strings strings', (done) => {
+            var { utr, spawn } = _makeUtr();
             spawn.setDefault(spawn.simple(0, '["command_line_text"]'));
             utr.history((histEntries: Array<string>) => {
                 assert.deepEqual(histEntries, ['command_line_text']);
@@ -39,14 +61,14 @@ describe('UTR tests', () => {
         });
     });
 
-    var _makeUtr = function() {
+    var _makeUtr = function () {
         const child_process = require('child_process')
         const mockSpawn = require('mock-spawn')
-        var spawn = mockSpawn ();
+        var spawn = mockSpawn();
         child_process.spawn = spawn;
         spawn.setDefault(spawn.simple(0));
 
-        const utr = new Utr("repo_root", spawn); 
-        return {utr: utr, spawn: spawn};
+        const utr = new Utr("repo_root", spawn);
+        return { utr: utr, spawn: spawn };
     }
 });
